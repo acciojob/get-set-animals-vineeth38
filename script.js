53905cec3e5321de559d74885c1910bc6100a934
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-05 11:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 class Animal {
-	constructor(species){
-		this.species = species;
-		getSpecies(){
-			return this.species;
-		}
-		makeSound(){
-			console.log("sound")
-		}
-	}
+  constructor(species) {
+    this.species = species;
+  }
+  get species() {
+    return this.species;
+  }
+  makeSound() {
+    console.log(`${this.species} makes a sound`);
+  }
 }
-
-
-
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
+  bark() {
+    console.log("woof");
+  }
 }
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
-
 
